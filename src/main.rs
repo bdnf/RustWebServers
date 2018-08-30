69ad68f60db9753b9bdc234b787e406c4ad48c6c
Oleh Bodunov <oleg.bdnf@gmail.com>
2018-08-30 13:31:46 +0200

    adding response

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs::File;
 
 fn handle_connection(mut stream: TcpStream) {
  let mut buffer_for_request_data = [0; 512];
-
  stream.read(&mut buffer_for_request_data).unwrap();
+ 
+ let mut file = File::open("./serve.html").unwrap();
+ let mut contents = String::new();
+ file.read_to_string(&mut contents).unwrap();
 
  //String::from_utf8_lossy function takes a &[u8] and produces a String from it.
- println!("Request: {}", String::from_utf8_lossy(&buffer_for_request_data[..]));
+ //println!("Request: {}", String::from_utf8_lossy(&buffer_for_request_data[..]));
+
+ //example response
+ //let response = "HTTP/1.1 200 OK\r\n\r\n";
+ let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+ stream.write(response.as_bytes()).unwrap();
+ stream.flush().unwrap();
 }
 
 fn main() {
